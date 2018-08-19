02b9e81aa987c2507caab1e1bb93ad2bf81dd804
Elle Mundy <exchgr@icloud.com>
2018-08-19 18:48:50 -0400

    User profile edit page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+end
